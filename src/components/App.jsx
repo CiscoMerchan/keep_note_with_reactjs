ba9158f447052d14f2ac15a22c3ed908046372e3
CiscoMerchan <franciscoeliasm@yahoo.com>
2023-05-10 14:40:28 +1000

    creation of App component

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App(){
+    return(
+    <><div>
+            <h1>Keep Note</h1>
+        </div>
+        <di>
+            <h2>Create Note</h2>
+        </di>
+    
+            </>
+    );
+}
+export default App;
