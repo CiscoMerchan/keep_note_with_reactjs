b7aaf8ca740e097cc15cefdd66f8a9271ff8b9cc
CiscoMerchan <franciscoeliasm@yahoo.com>
2023-05-11 11:38:38 +1000

    new array that will take the input from createNote and will be use to create new Note

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import CreateNote from "./CreateNote";
 import Note from "./Note";
 function App(){
 
+    /* add 'note' into a new array using state because needs to be change. at the begging is an empty array,
+    it will be populated once the user start to create notes in createNote component*/
+    const [notes, setNotes]=useState([])
+
     function addNote(note){
         console.log(note)
+        // populate 'notes'
+        setNotes(prevNotes =>{
+            return [...prevNotes, note];
+        })
     }
 
     return(
