5621aa3e09a81927fcc6e44ad7fdbd66f2ca810d
CiscoMerchan <franciscoeliasm@yahoo.com>
2023-05-11 11:56:05 +1000

    button to delete Note

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -3,10 +3,14 @@ import React from "react";
 // Note component it will render the input from CreateNote component
 
 function Note(props){
+    
+    function handleClick(){}
+    
     return(
         <div className="note">
             <h1>{props.title}</h1>
             <p>{props.content}</p>
+            <button onClick={handleClick}>DELETE</button>
         </div>
     )
 }
